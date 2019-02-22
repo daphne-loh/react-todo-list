29e1f35ac90b24b775a940182f272a903e688384
Angeline <atan@thoughtworks.com>
2019-02-22 16:51:19 +0800

    Create strike through style

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 function TodoItem({ id, name, isCompleted, handleClick }) {
-  
+  const getStyleCompleted = (value) => {
+    return value  && "line-through" 
+  }
   return (
     <div>
-      <li onClick={() => handleClick(id)}>{name}</li>
+      <li style={{"textDecoration" : getStyleCompleted(isCompleted) }} onClick={() => handleClick(id)}>{name}</li>
     </div>
   );
 }
