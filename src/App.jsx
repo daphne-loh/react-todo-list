cb5428562d859c244d839b06609b9e58bff7fa5f
Angeline <atan@thoughtworks.com>
2019-02-22 15:35:43 +0800

    Create TodoList component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { todos } from "./seedData";
+import { TodoList } from "./components/TodoList/TodoList";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Todo List</h1>
+        <TodoList data={todos}/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class TodoList extends Component {
+  state = {
+    data: this.props.data
+  }
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
+
+export default TodoList
