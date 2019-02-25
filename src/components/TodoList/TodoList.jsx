1f918446e0ca55f3710fd8eefb6ed84c8dba072f
Angeline <atan@thoughtworks.com>
2019-02-25 12:49:26 +0800

    Refactor TodoList from class to functional component

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React from 'react'
 import TodoItem from "../TodoItem/TodoItem";
 
-export class TodoList extends Component {
+export function TodoList({searchTerm, data, handleClick}) {
+  const filtering = item => item.name.includes(searchTerm)
 
-  render() {
-    const {searchTerm, data} = this.props
-    const filtering = item => item.name.includes(searchTerm)
-    return (
-      <ul className="list-group">
-        {data.filter(filtering).map(item => (
-          <TodoItem
-            id={item.id}
-            name={item.name}
-            isCompleted={item.isCompleted}
-            key={item.id}
-            handleClick={this.props.handleClick}
-          />
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className="list-group">
+      {data.filter(filtering).map(item => (
+        <TodoItem
+          id={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+          key={item.id}
+          handleClick={handleClick}
+        />
+      ))}
+    </ul>
+  );
 }
 
-export default TodoList;
+export default TodoList
