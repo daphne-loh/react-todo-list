2a175723701c68ee84580b3800e75ecf6e391147
Angeline <atan@thoughtworks.com>
2019-02-25 11:16:13 +0800

    Implement TodoCreationBar handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { cloneDeep } from "lodash";
 import { todos } from "./seedData";
 import { TodoList } from "./components/TodoList/TodoList";
 import TodoCreationBar from "./components/TodoCreationBar/TodoCreationBar";
@@ -9,30 +10,34 @@ class App extends Component {
     inputBarValue: ""
   };
 
-  handleClick = (itemId) => {
-    const {data} = this.state;
-    const updatedState = data.map( item => {
-      if(item.id === itemId){
-        const brandNew = {...item}
-        brandNew.isCompleted = !brandNew.isCompleted
-        return brandNew
+  handleClick = itemId => {
+    const { data } = this.state;
+    const updatedState = data.map(item => {
+      if (item.id === itemId) {
+        const brandNew = { ...item };
+        brandNew.isCompleted = !brandNew.isCompleted;
+        return brandNew;
       }
-      return item
+      return item;
     });
 
-    this.setState({data: updatedState})
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("handle submit");
-    //get value and create a new item
-    
-
+    this.setState({ data: updatedState });
   };
 
-  handleChange = (event) => {
-    this.setState({inputBarValue: event.target.value})
+  handleSubmit = event => {
+    event.preventDefault();
+    const {data, inputBarValue} = this.state;
+    const copy = cloneDeep(data);
+    copy.push({
+      id: data.length + 1,
+      name: inputBarValue,
+      isCompleted: false
+    });
+    this.setState({data: copy})
+  };
+
+  handleChange = event => {
+    this.setState({ inputBarValue: event.target.value });
   };
 
   render() {
@@ -46,7 +51,7 @@ class App extends Component {
           handleChange={this.handleChange}
           inputBarValue={inputBarValue}
         />
-        <TodoList data={data} handleClick={this.handleClick}/>
+        <TodoList data={data} handleClick={this.handleClick} />
       </React.Fragment>
     );
   }
